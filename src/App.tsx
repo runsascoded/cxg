a1ab0888308b187a05bff7afe1cc697638df58c9
Ryan Williams <ryan.williams@tiledb.com>
2025-04-27 00:08:33 -0400

    wip plot controls

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ import { useMemo, useState } from "react";
 const { log10, min, max } = Math
 
 export type Species = 'homo_sapiens' | 'mus_musculus'
+export const Species = [ 'homo_sapiens', 'mus_musculus' ]
 export type Census = '2023-05-15' | '2024-07-01' | '2025-01-30'
+export const CensusVersions: Census[] = ['2023-05-15', '2024-07-01', '2025-01-30']
 
 function humanize(num: number): string {
   if (num >= 1e6) {
@@ -17,32 +19,91 @@ function humanize(num: number): string {
   }
 }
 
+function titleCase(s: string): string {
+  return s.split(/[_ ]/).map(w => w.charAt(0).toUpperCase() + w.slice(1)).join(' ')
+}
+
+function unsnake(s: string): string {
+  return s.replace('_', ' ')
+  // return s.split(/[_ ]/).map(w => w.charAt(0).toUpperCase() + w.slice(1)).join(' ')
+}
+
 function App() {
   const [ species, setSpecies ] = useState<Species>('homo_sapiens')
   const [ census, setCensus ] = useState<Census>('2025-01-30')
-  const [ isPrimary, setIsPrimary ] = useState<boolean>(true)
-  const { hist, nCells } = useMemo(
+  const [ isPrimary, setIsPrimary ] = useState<boolean | null>(true)
+  const { hist, nCells, totalCells, } = useMemo(
     () => {
-      const hist = censusData.filter(
+      let hist = censusData.filter(
         o =>
           o.species === species &&
           o.census === census &&
-          o.is_primary === isPrimary
+          (isPrimary === null || o.is_primary === isPrimary)
       ).map(({ tissue, n_cells }) => ({
-        tissue: tissue.split(' ').map(s => s.charAt(0).toUpperCase() + s.slice(1)).join(' '),
+        tissue: titleCase(tissue),
         n_cells,
       }))
+      let byTissue: Record<string, number> = {}
+      hist.forEach(({ tissue, n_cells }) => {
+        if (!(tissue in byTissue)) {
+          byTissue[tissue] = 0
+        }
+        byTissue[tissue] += n_cells
+      })
+      hist = Object.entries(byTissue).map(([ tissue, n_cells ]) => ({ tissue, n_cells }))
       hist.sort((a, b) => a.n_cells - b.n_cells)
       const nCells = hist.map(({ n_cells }) => n_cells)
-      return { hist, nCells }
+      const totalCells = nCells.reduce((a, b) => a + b, 0)
+      return { hist, nCells, totalCells }
     },
-    [species, census]
+    [species, census, isPrimary]
   )
   const range = [log10(nCells.reduce((a, b) => min(a, b))) - .4, log10(nCells.reduce((a, b) => max(a, b), 0)) + .5]
   // console.log(censusData)
+  // const primary = isPrimary !== false
+  // const secondary = isPrimary !== true
+  const isPrimaryStr = { null: 'All', true: 'Primary', false: 'Secondary' }[isPrimary]
+  // console.log("primary:", primary, "secondary:", secondary, "isPrimary:", isPrimary)
   return (
     <>
       <h2>CELLxGENE Census Cell-Tissue Counts</h2>
+      <p>
+        <select
+          value={census}
+          onChange={e => setCensus(e.target.value as Census)}
+        >{
+          CensusVersions.map(v => <option key={v} value={v}>{v}</option>)
+        }</select> census,
+        <select
+          value={species}
+          onChange={e => setSpecies(e.target.value as Species)}
+        >{
+          Species.map(v => <option key={v} value={v}>{unsnake(v)}</option>)
+        }</select>
+        <select value={isPrimaryStr} onChange={e => setIsPrimary(e.target.value === 'All' ? null : e.target.value === 'Primary')
+        }>
+          <option value={"All"}>All</option>
+          <option value={"Primary"}>Primary only</option>
+          <option value={"Secondary"}>Secondary only</option>
+        </select>
+        {/*<label>*/}
+        {/*  <input type={"checkbox"} checked={primary} onChange={e => {*/}
+        {/*    const primary = e.target.checked*/}
+        {/*    console.log("setting primary:", primary)*/}
+        {/*    setIsPrimary(primary && secondary ? null : primary)*/}
+        {/*  }}/>*/}
+        {/*  Primary*/}
+        {/*</label>*/}
+        {/*<label>*/}
+        {/*  <input type={"checkbox"} checked={secondary} onChange={e => {*/}
+        {/*    const secondary = e.target.checked*/}
+        {/*    console.log("setting secondary:", secondary)*/}
+        {/*    setIsPrimary(primary && secondary ? null : secondary)*/}
+        {/*  }}/>*/}
+        {/*  Secondary*/}
+        {/*</label>*/}
+      </p>
+      <p>{totalCells} total cells</p>
       <div>
         <Plot
           data={[
@@ -59,14 +120,21 @@ function App() {
             },
           ]}
           layout={{
-            height: 1000,
+            height: 900,
             width: 600,
-            margin: { l: 165, t: 10, b: 30, r: 10 },
+            margin: { l: 145, t: 10, b: 30, r: 10 },
             xaxis: {
               type: 'log',
               tickformat: '.0s',
               range,
               title: { standoff: 20 },
+            },
+            yaxis: {
+              dtick: 1,
+              ticksuffix: ' ',
+              tickfont: {
+                size: 10,
+              },
             },
             // title: { text: 'A Fancy Plot' },
           }}
